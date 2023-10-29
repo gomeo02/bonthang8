b9bc01144c1821c387c9b1a76f72ff2d839ef8b3
Hoang Thi Xa <137850069+gomeo02@users.noreply.github.com>
2023-10-29 09:51:38 +0700

    Create push.go

---
+++ push.go
@@ -0,0 +1,12 @@
+Manchester derby: The questions Manchester United and Manchester City must answer
+It is beyond question that in the decade since Sir Alex Ferguson retired, Manchester City have been better than Manchester United.
+
+City have won six Premier League titles in that time, United have never even challenged.
+
+Under Pep Guardiola, the Blues completed a domestic Treble in 2018-19 and the real one last season, accounting for United in the FA Cup final thanks to two goals from Ilkay Gundogan.
+
+United have never finished above City in that time - they did finish level on points in 2015-16 but both were well behind eventual champions Leicester. The average points gap between them in the past 10 years is 16.5. No matter what the outcome at Old Trafford on Sunday, Guardiola's side will still be above Erik ten Hag's in the table.
+
+Yet, despite their superiority, City have only recorded three derby doubles against United in the Premier League during that time. The number of wins in the past 20 league meetings stands at 10-7 to City.
+
+So the outcome of the latest all-Manchester skirmish at Old Trafford is not a foregone conclusion, even without the air of uncertainty that hangs over both sides going into Sunday's game.
